77456d4bd3e67aa98e60761000cbcc2e232eae7b
john7319 <jonniekaras@gmail.com>
2024-03-12 22:17:57 +0300

    added and removed elements from an array in index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+// adds an element to the end of the array
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+// adds an element at the beginning of an array
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+//removes an element at the end of the array
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+//removes the first element
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+   return [...cats, name ]
+}
+
+
+function prependCat(name){
+    return [name ,...cats ]
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
